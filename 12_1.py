dccf8d0f9e61a72703916ac2929457807d040308
S John Trombley <s.john.trombley@gmail.com>
2024-01-03 00:21:53 -0700

    day 12 part 1 finished

---
+++ 12_1.py
@@ -0,0 +1,32 @@
+from itertools import combinations, groupby
+
+with open('12_input.txt') as f:
+    rows = []
+    for line in f:
+        row, group_sizes = line.split(' ')
+        rows.append((row, [int(s) for s in group_sizes.split(',')]))
+#rows = [
+#    ('???.###', [1, 1, 3]),
+#    ('.??..??...?##.', [1, 1, 3]),
+#    ('?#?#?#?#?#?#?#?', [1, 3, 1, 6]),
+#    ('????.#...#...', [4, 1, 1]),
+#    ('????.######..#####.', [1, 6, 5]),
+#    ('?###????????', [3, 2, 1]),
+#]
+
+possible_count = 0
+for row, group_sizes in rows:
+    unknown_count = row.count('?')
+    unknown_damage_count = sum(group_sizes) - row.count('#')
+    for possible_damaged in combinations(range(unknown_count), unknown_damage_count):
+        possible_row = row
+        i = 0
+        for d in possible_damaged:
+            possible_row = possible_row.replace('?', '.', d-i).replace('?', '#', 1)
+            i = d + 1
+        possible_row = possible_row.replace('?', '.')
+        if [len(list(g)) for k, g in groupby(possible_row) if k == '#'] == group_sizes:
+            possible_count += 1
+print(possible_count)
+
+
