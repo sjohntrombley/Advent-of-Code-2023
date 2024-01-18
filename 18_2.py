9a4478f6a7dd3d98aa43324a93f60d8b91c0e498
S John Trombley <s.john.trombley@gmail.com>
2024-01-18 00:20:47 -0700

    minor improvements but still bad

--- 18_2.py
+++ 18_2.py
@@ -1,18 +1,34 @@
+#example_dig_plan = [
+#    (0, int('70c71', 16)),
+#    (1, int('0dc57', 16)),
+#    (0, int('5713f', 16)),
+#    (1, int('d2c08', 16)),
+#    (0, int('59c68', 16)),
+#    (1, int('411b9', 16)),
+#    (2, int('8ceee', 16)),
+#    (3, int('caa17', 16)),
+#    (2, int('1b58a', 16)),
+#    (1, int('caa17', 16)),
+#    (2, int('7807d', 16)),
+#    (3, int('a77fa', 16)),
+#    (2, int('01523', 16)),
+#    (3, int('7a21e', 16))
+#]
 example_dig_plan = [
-    (0, int('70c71', 16)),
-    (1, int('0dc57', 16)),
-    (0, int('5713f', 16)),
-    (1, int('d2c08', 16)),
-    (0, int('59c68', 16)),
-    (1, int('411b9', 16)),
-    (2, int('8ceee', 16)),
-    (3, int('caa17', 16)),
-    (2, int('1b58a', 16)),
-    (1, int('caa17', 16)),
-    (2, int('7807d', 16)),
-    (3, int('a77fa', 16)),
-    (2, int('01523', 16)),
-    (3, int('7a21e', 16))
+    (2, 6),
+    (3, 5),
+    (0, 2),
+    (3, 2),
+    (2, 2),
+    (3, 2),
+    (0, 5),
+    (1, 2),
+    (0, 1),
+    (1, 2),
+    (2, 2),
+    (1, 3),
+    (0, 2),
+    (1, 2)
 ]
 dir_map = {'U': 3, 'R': 0, 'D': 1, 'L': 2}
 
@@ -33,23 +49,14 @@ def solve(dig_plan):
         for src_edge, dest_edge in zip(dig_plan, dig_plan[1:])
     ) // 3
 
-    d0 = dig_plan[-1][0]
-    d1 = dig_plan[0][0]
-    # if clockwise loop:
-    #   if clockwise turn:
-    #       F: (0, 0)
-    #       7: (0, 1)
-    #       J: (1, 1)
-    #       L: (1, 0)
-    vertices = [(
-        (1 + loop_dir*((d0-d1)%4 - 2)*((d1&2)-1))//2,
-        (1 + loop_dir*((d0-d1)%4 - 2)*(1-(d0&2)))//2
-    )]
-    for (d0, l0), (d1, _) in zip(dig_plan, dig_plan[1:]):
+    vertices = [(1, 1)]
+    for direction, length in dig_plan:
         vertices.append((
-            vertices[-1][0]
+            vertices[-1][0] + 2*(direction&1)*(1-(direction&2))*length,
+            vertices[-1][1] + (direction&2)*(1-2*(direction&1))*length
         ))
+    print([(r/2, c/2) for r, c in vertices])
 
 
 if __name__ == '__main__':
-    print(example_dig_plan)
+    solve(example_dig_plan)
